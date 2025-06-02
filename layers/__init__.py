e98000324b54d18305f8cc5be5b1ff1e9737b66d
Le Trung Kien-desktop <klynx1311@gmail.com>
2025-06-02 13:09:37 +0700

    them path vao init layer

--- layers/__init__.py
+++ layers/__init__.py
@@ -3,5 +3,7 @@ from ..src.layers.core.dense import Dense
 from ..src.layers.convolutional.conv2d import Conv2D
 from ..src.layers.activations.activation import Activation
 from ..src.layers.reshaping.flatten import Flatten
+from ..src.layers.pooling.max_pooling2d import MaxPooling2D
+from ..src.layers.pooling.global_max_pooling2d import GlobalMaxPooling2D
 
-__all__ = ["Layer", "Dense", "Conv2D", "Activation", "Flatten"]
+__all__ = ["Layer", "Dense", "Conv2D", "Activation", "Flatten", "MaxPooling2D", "GlobalMaxPooling2D"]
