43952f5bfc7742153e319c1ef3178109911a1e4c
Le Trung Kien-desktop <klynx1311@gmail.com>
2025-06-02 11:46:24 +0700

    sua lai lop sigmoid theo chuan layer

--- src/layers/activations/sigmoid.py
+++ src/layers/activations/sigmoid.py
@@ -2,6 +2,9 @@ import numpy as np
 from ..base import Layer
 
 class Sigmoid(Layer):
+    def __init__(self, name=None):
+        super().__init__(name=name)
+        
     def forward(self, x):
         self.output = 1 / (1 + np.exp(-x))
         return self.output
