5ca7e25ba8b4c85cc740822aa9f875562bb81edd
Le Trung Kien-desktop <klynx1311@gmail.com>
2025-06-02 18:38:40 +0700

    them optimizer adam va sua lai register

--- registry.py
+++ registry.py
@@ -1,15 +1,23 @@
 from .src.losses.mse import MSELoss
+from .src.losses.categorical_crossentropy import CategoricalCrossentropy
+from .src.losses.binary_crossentropy import BinaryCrossentropy
+
 from .src.optimizers.sgd import SGD
+from .src.optimizers.adam import Adam
+
 from .src.layers.activations.relu import ReLU
 from .src.layers.activations.sigmoid import Sigmoid
 from .src.layers.activations.tanh import Tanh
 
 LOSS_REGISTRY = {
     "mse": MSELoss,
+    "categorical_crossentropy": CategoricalCrossentropy,
+    "binary_crossentropy": BinaryCrossentropy,
 }
 
 OPTIMIZER_REGISTRY = {
     "sgd": SGD,
+    "adam": Adam,
 }
 
 ACTIVATION_REGISTRY = {
@@ -28,10 +36,10 @@ def get_optimizer(identifier):
         return OPTIMIZER_REGISTRY[identifier.lower()]()
     return identifier
 
-def get_activation(identifier):
+def get_optimizer(identifier, **kwargs):
     if isinstance(identifier, str):
-        act_cls = ACTIVATION_REGISTRY.get(identifier.lower())
-        if act_cls is None:
-            raise ValueError(f"Unknown activation '{identifier}'")
-        return act_cls()
-    return identifier
+        opt_cls = OPTIMIZER_REGISTRY.get(identifier.lower())
+        if opt_cls is None:
+            raise ValueError(f"Unknown optimizer '{identifier}'")
+        return opt_cls(**kwargs)
+    return identifier 
